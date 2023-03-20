3cc7063774a55f19a5348c5ca26d1ed3e1e3d30e
Jose David Martinez Romero <direccion correo>
2023-03-20 11:49:03 +0100

    Posibilidad de editar a un jugador(Sin probar)

--- front-end/static-files/js/Equipo_Jugadores.js
+++ front-end/static-files/js/Equipo_Jugadores.js
@@ -50,3 +50,45 @@ Jugadores.mostrarTabla = function (jugador) {
         + Jugadores.TablaEquipo.cuerpo
         + Jugadores.TablaEquipo.pie;
 }
+
+
+/// Plantilla para poner los datos de una persona en un tabla dentro de un formulario
+Jugadores.FormularioJugador = {}
+
+
+// Cabecera del formulario
+Jugadores.EditarJugador.formulario = `
+<form method='post' action=''>
+    <table width="100%" class="listado-jugadores">
+        <thead>
+            <th width="10%">Dorsal</th><th width="20%">Nombre</th><th width="20%">Apellidos</th><th width="10%">Equipo</th>
+            <th width="15%">Año contratación</th><th width="25%">posicion</th>
+        </thead>
+        <tbody>
+            <tr title="${Jugadores.Tags.dorsal}">
+                <td><input type="text" class="form-persona-elemento" disabled id="form-jugadores-dorsal"
+                        value="${Jugadores.Tags.dorsal}" 
+                        name="dorsal_jugador"/></td>
+                <td><input type="text" class="form-jugador-elemento editable" disabled
+                        id="form-jugador-dorsal" required value="${Jugadores.Tags.dorsal}" 
+                        name="dorsal_jugador"/></td>
+                <td><input type="text" class="form-jugador-elemento editable" disabled
+                        id="form-jugador_nombre" value="${Jugadores.Tags.nombre}" 
+                        name="nombre_jugador"/></td>
+                <td><input type="text" class="form-jugador-elemento editable" disabled
+                        id="form-jugador_apellido" required value="${jugador.Tags.apellidos}" 
+                        name="apellido_jugador"/></td>
+                <td><input type="number" class="form-jugador-elemento editable" disabled
+                        id="form-jugador-anio" min="1950" max="2030" size="8" required
+                        value="${Jugadores.Tags["AÑO ENTRADA"]}" 
+                        name="año_entrada_jugador"/></td>
+                <td>
+                    <div><a href="javascript:Jugador.editar()" class="opcion-secundaria mostrar">Editar</a></div>
+                    <div><a href="javascript:Jugador.guardar()" class="opcion-terciaria editar ocultar">Guardar</a></div>
+                    <div><a href="javascript:Jugador.cancelar()" class="opcion-terciaria editar ocultar">Cancelar</a></div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</form>
+`;
